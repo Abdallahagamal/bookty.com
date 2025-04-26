029d6ed55ab8343990c555eef81923237c50f8c5
Abdallah Gamal <abdallahgamal987@gmail.com>
2025-04-26 03:47:49 +0300

    edit book page

--- WEB assignment/book_page/bookpage.js
+++ WEB assignment/book_page/bookpage.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const title = localStorage.getItem("bookTitle");
     const image = localStorage.getItem("bookImage");
     const cate = localStorage.getItem("bookcate");
+    const decription = localStorage.getItem("bookdecription");
+    const avaliability = localStorage.getItem("bookavaliability2");
 
     if (title) {
         let titleEl = document.querySelector(".book-title");
@@ -19,6 +21,101 @@ document.addEventListener("DOMContentLoaded", () => {
         let cateEl = document.querySelector(".type-book");
         if (cateEl) cateEl.textContent = cate;
     }
-    localStorage.clear();
 
+    if (title) {
+        let bookTitleEl = document.querySelector(".book-title");
+        if (bookTitleEl) bookTitleEl.textContent = title;
+    }
+    if (decription) {
+        let bookDescriptionEl = document.querySelector(".content");
+        if (bookDescriptionEl) bookDescriptionEl.textContent = decription;
+        
+    }
+    if(avaliability==="false"){
+        const bookAvaliabilityEl = document.getElementById("image");
+        const bookava= document.getElementById("aval");
+        if (bookAvaliabilityEl) bookAvaliabilityEl.src = '../Group 18.png';
+        if (bookava) bookava.textContent = "Not Available";
+        document.getElementById("b11").classList.remove('active');
+    }
+    else{
+        console.log("not available")
+        document.getElementById("b11").classList.add('active');
+
+    }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll('.book-tab');
+    tabs[0].classList.add('active');
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            // Remove 'active' from all buttons
+            tabs.forEach(btn => btn.classList.remove('active'));
+            // Add 'active' only to the clicked one
+            tab.classList.add('active');
+        });
+    });
+}); 
+
+
+function showTab(tabName) {
+    const infoTab = document.getElementById('info-box');
+    const similarTab = document.getElementById('similar-box');
+  
+    if (tabName === 'info') {
+      infoTab.style.display = 'block';
+      similarTab.style.display = 'none';
+    } else if (tabName === 'similar') {
+      infoTab.style.display = 'none';
+      similarTab.style.display = 'block';
+    }
+  }
+  
+function handleClick(element) {
+    const newtitle = element.getAttribute("data-title");
+    const newimage = element.getAttribute("src");
+    const newcate = element.getAttribute("data-cate");
+    const newdecription = element.getAttribute("data-desc");
+    const avaliability = element.getAttribute("data-av");
+    if (newtitle) {
+        let titleEl = document.querySelector(".book-title");
+        if (titleEl) titleEl.textContent = newtitle;
+    }
+
+    if (newimage) {
+        let bookCoverEl = document.querySelector(".book-cover");
+        if (bookCoverEl) bookCoverEl.src = '../dadofinal/' + newimage;
+
+        let topBox = document.querySelector(".top-box");
+        if (topBox) topBox.style.backgroundImage = `url('../dadofinal/${newimage}')`;
+    }
+    if (newcate) {
+        let cateEl = document.querySelector(".type-book");
+        if (cateEl) cateEl.textContent = newcate;
+    }
+
+    if (newtitle) {
+        let bookTitleEl = document.querySelector(".book-title");
+        if (bookTitleEl) bookTitleEl.textContent = newtitle;
+    }
+    if (newdecription) {
+        let bookDescriptionEl = document.querySelector(".content");
+        if (bookDescriptionEl) bookDescriptionEl.textContent = newdecription;
+        
+    }
+    if(avaliability==="false"){
+        const bookAvaliabilityEl = document.getElementById("image");
+        const bookava= document.getElementById("aval");
+        if (bookAvaliabilityEl) bookAvaliabilityEl.src = '../Group 18.png';
+        if (bookava) bookava.textContent = "Not Available";
+        document.getElementById("b11").classList.remove('active');
+    }
+    else{
+        document.getElementById("b11").classList.add('active');
+        if (bookAvaliabilityEl) bookAvaliabilityEl.src = 'circle-checked.png';
+        if (bookava) bookava.textContent = "Available";
+
+    }
+
+}
